745c96d1984ebe738dab8f6f463cc03fcf1198d1
Alexandre Souza <alexandre.souza@live.com>
2025-10-29 02:57:06 -0300

    feat: all branches merge

--- ecommerce-api/src/main/java/br/com/ecommerce/config/AppConfig.java
+++ ecommerce-api/src/main/java/br/com/ecommerce/config/AppConfig.java
@@ -5,14 +5,8 @@ import org.springframework.context.annotation.Bean;
 
 
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
-=======
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
->>>>>>> f5c82d88e7d3da48b09ba93edd5c56a7d5410f71
 
 @Configuration
 public class AppConfig {
@@ -21,15 +15,9 @@ public class AppConfig {
 	BCryptPasswordEncoder criptografar() {
 		return new BCryptPasswordEncoder();
 	}
-<<<<<<< HEAD
-	
-	
 
 	@Bean
-	public RestTemplate restTemplate() {
+	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-=======
->>>>>>> f5c82d88e7d3da48b09ba93edd5c56a7d5410f71
-
 }
